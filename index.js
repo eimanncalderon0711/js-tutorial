365d00f11530517a22d1b80d8fbb3ad7c601aae5
eimanncalderon0711 <calderon.eimann0711@gmail.com>
2026-03-04 14:47:41 +0800

    Exercise 1 accessing object attributes

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Hello, World! This is the main.js file.");
+const person = {
+    name: 'John Doe',
+    age: 30,
+    status: 'single',
+    favoriteColor: 'blue'
+}
+
+const car = {
+    manufacturer: 'Toyota',
+    model: 'Camry',
+    year: 2020,
+    color: 'red'
+}
+
+
+const books = [
+    {
+        title: 'The Great Gatsby',
+        author: 'F. Scott Fitzgerald',
+        year: 1925,
+        genre: 'Novel'
+    },
+    {
+        title: 'To Kill a Mockingbird',
+        author: 'Harper Lee',
+        year: 1960,
+        genre: 'Novel'
+    }
+]
+
